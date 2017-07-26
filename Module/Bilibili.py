6ed726d213cd5733ffff7530cd841013f03fff27
tzw0745 <tzw0745@163.com>
2017-07-26 12:23:41 +0800

    update bilibili module

--- Module/Bilibili.py
+++ Module/Bilibili.py
@@ -17,8 +17,13 @@ class Bilibili:
         self.session = requests.session()
 
     def get_bangumi_info(self, bangumi_id):
+        """
+        获取番剧信息
+        :param bangumi_id: 番剧id
+        :return: 字典信息，包括名称、简介、url列表
+        """
         bangumi_id = str(bangumi_id)
-        if re.match(r'\d+', bangumi_id):
+        if re.match(r'^\d+$', bangumi_id):
             host = 'http://bangumi.bilibili.com/'
             url = '{}jsonp/seasoninfo/{}.ver'.format(host, bangumi_id)
             r = self.session.get(url)
@@ -33,7 +38,7 @@ class Bilibili:
             eps = [{'title': x['index_title'], 'url': x['webplay_url']}
                    for x in data['episodes']]
             eps.reverse()
-        elif re.match(r'av\d+', bangumi_id):
+        elif re.match(r'^av\d+$', bangumi_id):
             host = 'http://www.bilibili.com/'
             url = '{}video/{}/'.format(host, bangumi_id)
             r = self.session.get(url)
@@ -45,10 +50,13 @@ class Bilibili:
             title = tree.cssselect('div.v\-title')[0]
             title = title.cssselect('h1')[0].text
             intro = tree.cssselect('div#v_desc')[0].text
-            select = tree.cssselect('select#dedepagetitles')[0]
-            eps = [{'title': x.text.split('、', 1)[-1],
-                    'url': parse.urljoin(url, x.get('value'))}
-                   for x in select.cssselect('option')]
+            select = tree.cssselect('select#dedepagetitles')
+            if select:
+                eps = [{'title': x.text.split('、', 1)[-1],
+                        'url': parse.urljoin(url, x.get('value'))}
+                       for x in select[0].cssselect('option')]
+            else:
+                eps = [{'title': title, 'url': url}]
         else:
             raise ValueError('bangumi id not match')
 
@@ -58,6 +66,15 @@ class Bilibili:
 
     @staticmethod
     def video_url_list(url, you_get=None):
+        """
+        获取网页中的视频文件列表
+        :param url: 页面url
+        :param you_get: you-get.exe 文件路径
+        :return: 视频文件列表和文件类型
+        """
+        if not url.startswith('http://bangumi.bilibili.com/anime/') and \
+                not url.startswith('http://www.bilibili.com/video/'):
+            raise ValueError('url not correct')
         you_get = you_get if you_get else 'you-get.exe'
         cmd = '{} --json {}'.format(you_get, url)
         output = check_output(cmd)
